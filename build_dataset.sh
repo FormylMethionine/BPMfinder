e497abb16dc37a67ee37a15086901bdff7e2d869
Vincent Le Goff <vb.legoff@free.fr>
2020-11-15 00:27:20 +0100

    correction on conversion to ogg files

--- build_dataset.sh
+++ build_dataset.sh
@@ -3,6 +3,8 @@
 OIFS="$IFS"
 IFS=$'\n'
 
+rm dataset_ddr/*
+
 cd "dataset_ddr/packs"
 
 if [ -d "../stepcharts" ]; then
@@ -15,6 +17,21 @@ if [ -d "../audiofiles" ]; then
 fi
 mkdir "../audiofiles"
 
+if [ -d "../train" ]; then
+	rm -rf "../train"
+fi
+mkdir "../train"
+
+if [ -d "../test" ]; then
+	rm -rf "../test"
+fi
+mkdir "../test"
+
+if [ -d "../val" ]; then
+	rm -rf "../val"
+fi
+mkdir "../val"
+
 for f in $(find -name "*.sm"); do
 	echo "Copying $f ..."
 	cp "$f" "../stepcharts"
@@ -22,12 +39,31 @@ done
 #echo $(ls -l ../stepcharts | wc -l) "files copied"
 
 for f in $(find -name "*.mp3" -o -name "*.wav"); do
-	ffmpeg -i $f -o ${f%%.*}
+	echo "Converting $f to ogg"
+	ffmpeg -hide_banner -loglevel quiet -n -i $f "${f%.*}.ogg"
+	rm $f
 done
 
-for f in $(find -name "*.ogg" -o -name "*.mp3" -o -name "*.wav"); do
+for f in $(find -name "*.ogg"); do
 	echo "Copying $f ..."
 	cp "$f" "../audiofiles"
 done
+
+cd ../..
+
+python parser.py
+
+rm -rf dataset_ddr/audiofiles/
+rm -rf dataset_ddr/stepcharts/
+
+./create_index.sh ./dataset_ddr
+cd dataset_ddr
+python ../split.py
+rm -rf index.txt
+cd ..
+
+./create_index.sh dataset_ddr/train
+./create_index.sh dataset_ddr/val
+./create_index.sh dataset_ddr/test
 
 IFS="$OIFS"
