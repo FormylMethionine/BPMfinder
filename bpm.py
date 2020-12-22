ef23888fe050cd21ff1478442cd7f8c089323a85
Vincent Le Goff <vb.legoff@free.fr>
2020-12-22 20:16:59 +0100

    better way to find peaks

--- bpm.py
+++ bpm.py
@@ -17,8 +17,8 @@ model = BeatCNN()
 
 path = sys.argv[1]
 audio = analyze(path)
-#model(audio[:100])  # just to initialize weights, awkward and slow 
-model.load("./weights/600_16.wgh")
+#model(audio[:100])  # just to initialize weights, awkward and slow
+model.load("./weights/2000_16.wgh")
 
 pred = model(audio).numpy().astype('float64')[:, 0]
 f, Pxx = signal.periodogram(pred)
@@ -26,22 +26,24 @@ f = 5172*f
 
 pks = signal.find_peaks(Pxx)[0]
 pks = [f[pks] for pks in sorted(pks, key=lambda x: Pxx[x], reverse=True)
-       if 50 <= f[pks] <= 500]
+       if 50 <= f[pks] <= 500][:10]
 bpm = pks[0]
-while not int(bpm) <= 200:
-    bpm /= 2
-print(f"Predicted BPM: {int(bpm)}")
-print(audio.shape)
+i = 1
+for i in range(1, 5):
+    new_bpm = bpm/i
+    if int(new_bpm) <= 200 and round(new_bpm) in list(map(round, pks)):
+        break
+print(f"Predicted BPM: {round(new_bpm)}")
 
 f1, ax1 = plt.subplots()
 ax1.plot(f, Pxx)
 ax1.set_xlim(50, 500)
-ax1.set_ylim(0, .1)
+ax1.set_ylim(0, max(Pxx))
 ax1.set_title(f"Periodogram of {path.split('/')[-1]}")
 ax1.set_xlabel("BPMs")
 
 f2, ax2 = plt.subplots()
-spec = ax2.imshow(audio[4000:6000, :, 1].T, cmap="Greys", aspect='auto')
+spec = ax2.imshow(audio[3000:6000, :, 1].T, cmap="Greys", aspect='auto')
 ax2.set_title(f"Mel spectrogram and predicted beats of {path.split('/')[-1]}")
 ax2.set_ylim(79, 0)
 ax2.set_xlabel('Time [frame]')
@@ -54,7 +56,7 @@ secax = ax2.secondary_xaxis('top',
 secax.set_xlabel('Time [s]')
 
 ax3 = ax2.twinx()
-ax3.plot(pred[4000:6000], color='red', alpha=0.75)
+ax3.plot(pred[3000:6000], color='red', alpha=1)
 ax3.set_ylabel('Predicted probability of presence of a beat',
                color='red')
 ax3.tick_params('y', labelcolor='red')
